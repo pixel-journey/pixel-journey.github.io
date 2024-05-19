feb3417d22c0876f8a9b0fdfa573272e23c399aa
piga-commits <git@piga.one>
2024-05-19 12:19:41 +0200

    Update pixels.js

--- js/pixels.js
+++ js/pixels.js
@@ -45,7 +45,7 @@ $(function() {
     });
 
     // countdown date
-    var end = new Date("05/31/2024 23:59 PM"); // FORMAT: month/day/year time
+    var end = new Date("05/31/2024 11:59 PM"); // FORMAT: month/day/year time
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
