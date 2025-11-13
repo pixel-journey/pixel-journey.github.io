21c0d5b7a119770b0e9bb10e3127aa00567908c1
piga-commits <git@piga.one>
2025-11-13 01:52:33 +0100

    test

--- PxDie/_next/static/Ksh-Ap41fpW38tJG4eUXR/_buildManifest.js
+++ PxDie/_next/static/Ksh-Ap41fpW38tJG4eUXR/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PxDie//_next/:path+",
+        "destination": "/PxDie/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
